bab242413b384298fc6268e8a990cea3864d6bdc
Seekaplayer <44986525+seekaplayer@users.noreply.github.com>
2020-10-13 10:27:09 -0400

    added in navbar with logo

--- client/src/Components/Navigation/Navigation.js
+++ client/src/Components/Navigation/Navigation.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Logo from "../Templates/Logo";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./Navigation.css";
 const Navigation = () => {
   return (
     <>
       <Navbar bg="dark">
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <Logo />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
