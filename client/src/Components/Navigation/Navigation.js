3cb22795dc98693b70e53134f2b92af4ae858b50
Seekaplayer <44986525+seekaplayer@users.noreply.github.com>
2020-10-14 00:46:31 -0400

    finished with navigation

--- client/src/Components/Navigation/Navigation.js
+++ client/src/Components/Navigation/Navigation.js
@@ -1,22 +1,31 @@
 import React from "react";
 import Logo from "../Templates/Logo";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import "./Navigation.css";
 const Navigation = () => {
   return (
     <>
-      <Navbar bg="dark">
-        <Navbar.Brand href="/">
-          <Logo />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar expand="lg" sticky="top">
+        <Container>
+          <Navbar.Brand href="/">
+            <Logo />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#portfolio">About</Nav.Link>
+              <Nav.Link href="#portfolio">Services</Nav.Link>
+              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="#blog">Blog</Nav.Link>
+              <Nav.Link href="#blog">Twitch</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   );
