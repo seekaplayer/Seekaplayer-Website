7afa7767792916ba7354d2425f363070c3a52012
Seekaplayer <44986525+seekaplayer@users.noreply.github.com>
2020-10-18 03:32:09 -0400

    installed react-router and got the routing working

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -3,10 +3,32 @@ import React from "react";
 // Template Components
 import Header from "./Templates/Header";
 import Footer from "./Templates/Footer";
+// Pages
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Services from "../Pages/Services";
+import Portfolio from "../Pages/Portfolio";
+import Blog from "../Pages/Blog";
+import Contact from "../Pages/Contact";
+import PageNotFound from "../Pages/404";
+// React Router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
       <Header />
+
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/Portfolio" component={Portfolio} />
+          <Route path="/Blog" component={Blog} />
+          <Route path="/Contact" component={Contact} />
+          <Route path="*" component={PageNotFound} />
+        </Switch>
+      </Router>
 
       <Footer />
     </>
--- client/src/Components/Navigation/Navigation.js
+++ client/src/Components/Navigation/Navigation.js
@@ -18,13 +18,13 @@ const Navigation = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#services">Services</Nav.Link>
-              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-              <Nav.Link href="#blog">Blog</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="about">About</Nav.Link>
+              <Nav.Link href="services">Services</Nav.Link>
+              <Nav.Link href="portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="blog">Blog</Nav.Link>
               <Nav.Link href="https://twitch.tv/seekaplayer">Twitch</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link href="contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
